11ae858e99d2fed7dfcc1f201aa85b68ad4e6a83
jnevesthe <nevesjosemar9@gmail.com>
2025-06-28 19:31:17 +0100

    Update wsgi.py

--- codehub/wsgi.py
+++ codehub/wsgi.py
@@ -7,16 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codehub.settings')
 
-application = get_wsgi_application()
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
