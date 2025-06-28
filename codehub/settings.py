5055f1d140f3914c7e44ab88570acdc926a212d4
jnevesthe <nevesjosemar9@gmail.com>
2025-06-28 19:03:00 +0100

    Update settings.py

--- codehub/settings.py
+++ codehub/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paginas',  # Seu app
+    'paginas.apps.PaginasConfig',  # Seu app
 ]
 
 MIDDLEWARE = [
