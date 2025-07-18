b6b80846199a6a7e59de4797297ecca274d31bb4
jnevesthe <nevesjosemar9@gmail.com>
2025-07-18 14:49:08 +0100

    Update urls.py

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,21 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.urls import path
-from .views import InfoListView, Index, Sobre, Servicos, Contato 
+from .views import InfoListView, Index, Sobre, Servicos, Contato
+from django.http import FileResponse
+import os
 
-from . import views
-
-
-urlpatterns=[
-
+urlpatterns = [
     path('info/', InfoListView.as_view(), name='info'),
     path('', Index.as_view(), name='index'),
     path('sobre/', Sobre.as_view(), name='sobre'),
-    path('services/', views.Servicos, name='servicos'),
+    path('services/', Servicos, name='servicos'),
     path('contato/', Contato.as_view(), name='contato'),
 
+    # Rota para o ads.txt
+    path("ads.txt", lambda request: FileResponse(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "ads.txt"), "rb"))),
 ]
 
+# (opcional) se estiver servindo arquivos estáticos localmente em DEBUG
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
